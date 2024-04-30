9545094d839d18ad7c3de6ebcf40757decc1bc0c
rlrl9 <klala8@naver.com>
2024-04-30 23:03:47 +0900

    feat:learn select

--- tab.js
+++ tab.js
@@ -11,3 +11,24 @@ function tabOpen(i) {
   });
   document.querySelectorAll(".tab-content")[i].classList.add("show");
 }
+var formSelects = document.querySelectorAll(".form-select");
+var shirts=[95,100,105];
+var pants=[28,29,30];
+formSelects[0].addEventListener("click", function (e) {
+  var value = e.currentTarget.value;
+  var formSelect2 = formSelects[1];
+  if (value === '셔츠') {
+    formSelect2.classList.remove("form-hide");
+    formSelect2.innerHTML='';
+    shirts.forEach(function(a){
+      formSelect2.insertAdjacentHTML('beforeend', `<option>${a}</option>`);
+    })  } else if (value === '모자') {
+    formSelect2.classList.add("form-hide");
+  } else if (value === '바지'){
+    formSelect2.classList.remove("form-hide");
+    formSelect2.innerHTML='';
+    pants.forEach(function(a){
+      formSelect2.insertAdjacentHTML('beforeend', `<option>${a}</option>`);
+    })
+  }
+});
